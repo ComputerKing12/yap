009fe7467a69aa76b40b87a663a03f70c84cfca9
ComputerKing12 <60110285+ComputerKing12@users.noreply.github.com>
2023-01-22 22:35:11 -0500

    added cpr

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <yap.h>
+#include <cpr/cpr.h>
 
 int main()
 {
 	yap::yap yap;
 	yap.init();
+
+	cpr::Response r = cpr::Get(
+		cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+		cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+		cpr::Parameters{{"anon", "true"}, {"key", "value"}}
+	);
+
+	std::cout << r.status_code << std::endl;
+
 	return 0;
 }
